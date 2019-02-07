7f19ea3422538eb49ff2f96032e6146a1f4c2b25
johnsk8 <Ludin@DESKTOP-BAB0NU4.(none)>
2019-02-06 21:07:08 -0800

    First commit test

--- source/PlayniteUI/Extensions.cs
+++ source/PlayniteUI/Extensions.cs
@@ -7,7 +7,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 
-namespace PlayniteUI
+namespace PlayniteUI //First commit test
 {
     public static class Extensions
     {
